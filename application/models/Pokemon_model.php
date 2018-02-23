921bc61dbfd35363afe9dda3aa1fad00d92a4e57
Pierre Bretéché <pbreteche@dawan.fr>
2018-02-23 14:20:00 +0100

    ajout de relation

--- application/models/Pokemon_model.php
+++ application/models/Pokemon_model.php
@@ -11,13 +11,26 @@ class Pokemon_model extends CI_Model
 	public function getList()
 	{
 		$query = $this->db->get('pokemon', 30);
-
 		return $query->result();
 	}
 
 	public function get($id)
 	{
 		$query = $this->db->get_where('pokemon', ['id' => $id]);
-		return $query->result()[0] ?? null;
+
+		$pokemon = $query->result()[0] ?? null;
+
+		if ($pokemon) {
+			$this->db->select('*');
+			$this->db->from('skill s');
+			$this->db->join('has_skill hs', 's.id = hs.skill_id');
+			$this->db->where('hs.pokemon_id', $pokemon->id);
+			$query = $this->db->get();
+			$pokemon->skills = [];
+			foreach ($query->result() as $skill) {
+				$pokemon->skills[] = $skill;
+			}
+		}
+		return $pokemon;
 	}
 }
--- application/views/pokemons/detail.php
+++ application/views/pokemons/detail.php
@@ -4,5 +4,9 @@
 	<dt>Taille</dt><dd><?= $pokemon->size ?></dd>
 	<dt>Poids</dt><dd><?= $pokemon->weight ?></dd>
 	<dt>Genre</dt><dd><?= $pokemon->gender ?></dd>
-	<dt>Category</dt><dd><?= $pokemon->category ?></dd>
+	<dt>Catégorie</dt><dd><?= $pokemon->category ?></dd>
+	<dt>Capacité</dt>
+	<?php foreach($pokemon->skills as $skill): ?>
+	<dd><?= $skill->name ?></dd>
+	<?php endforeach; ?>
 </dl>
